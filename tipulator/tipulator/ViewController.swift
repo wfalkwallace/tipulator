51207180009d2e8eb23ad19f82e12fe51b30d708
William Falk-Wallace <wgf2104@columbia.edu>
2014-12-14 21:20:04 -0800

    bootstraps ios template

---
+++ tipulator/tipulator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipulator
+//
+//  Created by William Falk-Wallace on 12/14/14.
+//  Copyright (c) 2014 William Falk-Wallace. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
